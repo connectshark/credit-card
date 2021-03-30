ed0252b05103f0ac21f43f80379ecc16608f3528
connectshark <bobhus394@gmail.com>
2021-03-30 11:41:00 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style lang="scss">
+body{
+  background: linear-gradient(to left, #283593, #1976d2);
+}
+</style>
